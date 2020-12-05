416708c2b22a02689255e67737cf0d6aa842150e
taylulz <taylulzcode@gmail.com>
2020-12-05 15:15:44 -0800

    add a For loop underneath For Each loop

--- GroceryList.cs
+++ GroceryList.cs
@@ -1,25 +1,40 @@
 using System;
 
-class GroceryList
+// class GroceryList // for each loop
+// {
+//   static void Main()  
+//   {
+//     string[] myGroceryList = { "eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+
+//     int [] groceryListPrices = { 3,6,4,2,4,4 };
+
+//     Console.WriteLine("My grocery list:");
+//     foreach (string groceryItem in myGroceryList) // "for each item in myGroceryList, name it groceryItem, then run the line of code between the curly braces (below)." 
+//     {
+//       Console.WriteLine(groceryItem);
+//     }
+
+//     int total = 0;
+//     foreach (int price in groceryListPrices)
+//     {
+//       total += price;
+//     }
+
+//     Console.WriteLine("Your total for this shopping trip will be $" + total);
+//   }
+
+// } 
+
+class GroceryList // for loop
 {
-  static void Main()
+  static void Main() 
   {
     string[] myGroceryList = { "eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
 
-    int [] groceryListPrices = { 3,6,4,2,4,4 };
-
     Console.WriteLine("My grocery list:");
-    foreach (string groceryItem in myGroceryList) // "for each item in myGroceryList, name it groceryItem, then run the line of code between the curly braces (below)." 
+    for (int index = 0; index < myGroceryList.Length; index++)
     {
-      Console.WriteLine(groceryItem);
+      Console.WriteLine(myGroceryList[index]);
     }
-
-    int total = 0;
-    foreach (int price in groceryListPrices)
-    {
-      total = total + price;
-    }
-
-    Console.WriteLine("Your total for this shopping trip will be $" + total);
   }
-} 
+}  
